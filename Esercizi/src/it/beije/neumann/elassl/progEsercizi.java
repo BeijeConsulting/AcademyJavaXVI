337c17b485d783064f6cbe9cb6476bf715c96ad7
youness <mm@mm-KLVL-WXX9>
2023-02-07 15:24:53 +0100

    init

---
+++ Esercizi/src/it/beije/neumann/elassl/progEsercizi.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package elassl;
+
+/**
+ * @author mm
+ *
+ */
+public class progEsercizi {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
