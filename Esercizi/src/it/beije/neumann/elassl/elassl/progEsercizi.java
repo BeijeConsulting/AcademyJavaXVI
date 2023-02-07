70ebd007b0cd0d39ed52ae9a82c0c81217721691
youness <mm@mm-KLVL-WXX9>
2023-02-07 15:32:15 +0100

    package elassl

--- Esercizi/src/it/beije/neumann/elassl/elassl/progEsercizi.java
+++ Esercizi/src/it/beije/neumann/elassl/elassl/progEsercizi.java
@@ -1,19 +1,12 @@
-/**
- * 
- */
 package elassl;
 
-/**
- * @author mm
- *
- */
 public class progEsercizi {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hi");
 
 	}
 
