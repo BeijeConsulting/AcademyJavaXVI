31923fa3dc1f6d62a5d0d6f1ed1d03a695a7e68f
nverzaschi <nverzaschi@gmail.com>
2023-02-10 18:28:56 +0100

    XMLManager2 added

--- Esercizi/src/it/beije/neumann/nicole/rubrica/XMLManager2.java
+++ Esercizi/src/it/beije/neumann/nicole/rubrica/XMLManager2.java
@@ -9,7 +9,6 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -22,14 +21,18 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
- * scrivo una mia rubrica su un file xml
+ * La classe implementa 3 metodi:
+ * 1) getChildElements : ritorna i figli degli elementi
+ * 
+ * 2)readRubricaXML : legge un file XML e aggiunge i suoi elementi (in questo caso i contatti)
+ *   ad una lista di contatti
+ *   
+ * 3)writeRubrica : scrive i contatti su un file xml
  * @author nverz
  *
  */
 
-
-
-public class XMLManager 
+public class XMLManager2 
 {
 	public static List<Element> getChildElements(Element e) {
 		List<Element> elements = new ArrayList<Element>();
@@ -43,7 +46,50 @@ public class XMLManager
 		
 		return elements;
 	}
+	
+	public static void readRubricaXML(String pathFile,List<Contatto> contacts) throws ParserConfigurationException, IOException, SAXException {
 
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		Document document = documentBuilder.parse(pathFile);
+		
+		Element rootElement = document.getDocumentElement();
+		
+		List<Element> elements = getChildElements(rootElement);
+		Contatto contatto = null;
+		for (Element el : elements) {
+			contatto = new Contatto();
+			System.out.println("eta' : " + el.getAttribute("eta"));
+			
+			List<Element> values = getChildElements(el);
+			for (Element v : values) {
+				System.out.println("node name: " + v.getNodeName());
+				switch (v.getNodeName()) {
+				case "nome":
+					contatto.setName(v.getTextContent());
+					break;
+				case "cognome":
+					contatto.setSurname(v.getTextContent());
+					break;
+				case "telefono":
+					contatto.setTelephone(v.getTextContent());
+					break;
+				case "email":
+					contatto.setEmail(v.getTextContent());
+					break;
+				case "note":
+					contatto.setNote(v.getTextContent());
+					break;
+				}
+			}
+			
+			contacts.add(contatto);
+		}
+
+		System.out.println(contatti);
+
+	}
+	
 	public static void writeRubricaXML(List<Contatto> contatti, String pathFile) throws SAXException, IOException, TransformerException 
 	{
 		
@@ -123,7 +169,6 @@ public class XMLManager
 		
 }
 
-	
 	public static void main(String [] args) throws ParserConfigurationException, SAXException, IOException, TransformerException
 	{
 
@@ -138,7 +183,7 @@ public class XMLManager
 		c1.setEmail("nverzaschi@gmail.com");
 		c1.setTelephone("3349003586");
 		c1.setNote("mamma");
-		contatti.add(c1);
+		
 		
 		Contatto c2= new Contatto();
 		c2.setName("Lidia");
@@ -146,10 +191,13 @@ public class XMLManager
 		c2.setTelephone("nessun telefono");
 		c2.setEmail("lilli@gmail");
 		c2.setNote("");
-		contatti.add(c2);
 		
+		readRubricaXML("C:\\Users\\nverz\\Music\\esercizio.\\rubrica2.xml",contatti);
+		contatti.add(c1);
+		contatti.add(c2);
 		writeRubricaXML(contatti,"C:\\Users\\nverz\\Music\\esercizio.\\rubrica.xml");
 		
 	}
+	
 
 }
