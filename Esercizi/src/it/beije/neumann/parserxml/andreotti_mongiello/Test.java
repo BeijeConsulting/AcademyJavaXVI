0abaf69ddd334a46230b5d836785349abfd145cd
francescoandreotti <francescoandreotti00@gmail.com>
2023-02-13 14:34:02 +0100

    isEndTag method

--- Esercizi/src/it/beije/neumann/parserxml/andreotti_mongiello/Test.java
+++ Esercizi/src/it/beije/neumann/parserxml/andreotti_mongiello/Test.java
@@ -4,20 +4,39 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
 
 	public static void main(String[] args) throws IOException {
-		File file = new File("/Users/francescoandreotti/git/AcademyJavaXVI/Esercizi/src/it/beije/neumann/parserxml/andreotti_mongiello/test_parser1.xml");
-		BufferedReader br = new BufferedReader(new FileReader(file));
-		String xml = "";
-        String st;
-        while ((st = br.readLine()) != null) {
-        	xml = xml + st;
-        	System.out.println(st);
-        }
-       
-        getRootElement(xml);
+//		String path = "/Users/francescoandreotti/git/AcademyJavaXVI/Esercizi/src/it/beije/neumann/parserxml/andreotti_mongiello/test_parser1.xml";
+//		File file = new File(path);
+//		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+//			String xml = "";
+//			String st;
+//			while ((st = br.readLine()) != null) {
+//				xml = xml + st;
+//				System.out.println(st);
+//			}
+//      
+//			getRootElement(xml);
+//			try {
+//				StringBuilder fileContent = XmlParser.fromFileToString(path);
+//				//System.out.println(fileContent);
+//				List<String> listTag = XmlParser.listTag( path );
+//				String rootElement = XmlParser.getRootElement( listTag );
+//				System.out.println("Root element: " + rootElement);
+//				System.out.println(listTag);
+//				getChildElements(listTag,"contatto");
+//				
+//				
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//		}
+		boolean test = isEndTag("contatto", "contatto");
+		System.out.println(test);
 	}
 	
 	public static String getRootElement(String st) {
@@ -37,5 +56,25 @@ public class Test {
 		}
 		System.out.println("Root: " + root);
 		return root;
+	}
+	
+	public static void getChildElements(List<String> elements, String name) {
+//		List<String> childElements = new ArrayList<String>();
+		outerloop:
+		for (int i = 0; i < elements.size(); i++) {
+			if(elements.get(i).equals(name)) {
+				for(int k = 0; k < elements.size(); k++) {
+					
+				}
+				break outerloop;
+			}
+		}
+	}
+	
+	public static boolean isEndTag(String tag, String primaryElement) {
+		String[] splitted = tag.split("");
+		splitted[0] = "";
+		tag = String.join("", splitted);
+		return tag.equals(primaryElement);
 	}
 }
