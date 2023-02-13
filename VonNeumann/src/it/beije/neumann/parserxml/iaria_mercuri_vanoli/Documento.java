faefe333277ba97bc1f6b87ca7e51016f5c01580
gianf <gianf@GF>
2023-02-13 15:09:42 +0100

    senza attribute

--- VonNeumann/src/it/beije/neumann/parserxml/iaria_mercuri_vanoli/Documento.java
+++ VonNeumann/src/it/beije/neumann/parserxml/iaria_mercuri_vanoli/Documento.java
@@ -14,32 +14,52 @@ public class Documento {
 	}
 	
 	private Elemento rootElement;
+	
+	public Elemento getRootElement() {
+		return rootElement;
+	}
 
 	public static Documento parse(String file) throws IOException {
 		Documento document = new Documento();
 		FileReader reader = new FileReader(file);
 		StringBuilder tag = null;
+		StringBuilder textContext = null;
 		List<Elemento> elementi = new ArrayList<>();
 		Elemento currentElement = null;
 		
-        int content;
-        while ((content = reader.read()) != -1) {
-        	if(content == '<' && (content = reader.read()) != '?' && content != '/') {
+        int content = 0;
+        while (content != -1) {
+        	if(content == '<' && (content = reader.read()) != '?') {
         		tag = new StringBuilder();
         		tag.append((char)content);
         		while((content = reader.read()) != '>') {
         			tag.append((char)content);
         		}
-        		currentElement = new Elemento(tag.toString(), null, null, currentElement);
-        		if(tag.charAt(0) == '/') {
-        			currentElement = currentElement.getParent();
-        			//Elemento e = new Elemento(tag.toString(), null, null, );
-        			//document.rootElement.addFiglio(e);
-        			elementi.add(new Elemento(tag.toString(), null, null, currentElement));
+        		textContext = new StringBuilder();
+        		while((content = reader.read()) != '<' && content != -1) {
+        			textContext.append((char)content);
         		}
+        		if(tag.charAt(0) == '/') {
+        			if(currentElement.getParent() == null)
+        				document.rootElement = currentElement;
+        			currentElement = currentElement.getParent();
+        		} else {
+        			elementi.add(new Elemento(tag.toString(), textContext.toString(), null, currentElement)); //Lista test momentaneo
+        			Elemento e = new Elemento(tag.toString(), textContext.toString(), null, currentElement);
+        			if(currentElement != null)
+        				currentElement.addFiglio(e);
+        			currentElement = e;
+        		}
+        	} else {
+        		content = reader.read();
         	}
         }
-        System.out.println(elementi.toString());
+        System.out.println(document.rootElement);
+        for (Elemento e: elementi) {
+        	if(e.getParent() != null)
+        		System.out.println(e.getTag() + " e' figlio di " + e.getParent().getTag() + " con context: " + e.getTextContext());
+        }
+        //System.out.println(elementi.toString());
         
 
 		
@@ -48,6 +68,11 @@ public class Documento {
 	
 	public static void main(String[] args) throws IOException {
 		parse("/temp/test_parser1.xml");
+		
+		Documento document = parse("/temp/test_parser1.xml");
+		document.getRootElement().stampaAlbero();
+		//for(Elemento e: document.getRootElement().getChildElements())
+			//System.out.println(e.getChildElements());
 	}
 	
 }
