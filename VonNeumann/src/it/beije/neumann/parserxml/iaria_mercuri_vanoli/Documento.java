411f09528928c568d2a260a92c1dcb4fcb239883
gianf <gianf@GF>
2023-02-13 14:00:24 +0100

    Aggiornamento pre_pranzo

--- VonNeumann/src/it/beije/neumann/parserxml/iaria_mercuri_vanoli/Documento.java
+++ VonNeumann/src/it/beije/neumann/parserxml/iaria_mercuri_vanoli/Documento.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Documento {
 	
@@ -11,28 +13,35 @@ public class Documento {
 		
 	}
 	
-	//private Elemento rootElement;
-	private String rootElement = "";
+	private Elemento rootElement;
 
 	public static Documento parse(String file) throws IOException {
 		Documento document = new Documento();
 		FileReader reader = new FileReader(file);
 		StringBuilder tag = null;
+		List<Elemento> elementi = new ArrayList<>();
+		Elemento currentElement = null;
 		
         int content;
         while ((content = reader.read()) != -1) {
-        	if(content == '<' && (content = reader.read()) != '?') {
+        	if(content == '<' && (content = reader.read()) != '?' && content != '/') {
         		tag = new StringBuilder();
         		tag.append((char)content);
         		while((content = reader.read()) != '>') {
         			tag.append((char)content);
         		}
+        		currentElement = new Elemento(tag.toString(), null, null, currentElement);
+        		if(tag.charAt(0) == '/') {
+        			currentElement = currentElement.getParent();
+        			//Elemento e = new Elemento(tag.toString(), null, null, );
+        			//document.rootElement.addFiglio(e);
+        			elementi.add(new Elemento(tag.toString(), null, null, currentElement));
+        		}
         	}
         }
-        System.out.println(tag);
+        System.out.println(elementi.toString());
         
-        //root
-        
+
 		
 		return document;
 	}
