e54ca007f3803ee454c4c9e8454cc924bd176572
mongipi <pietromongiello@gmail.com>
2023-02-13 10:48:28 +0100

    implementata funzione getRootElement

--- Esercizi/src/it/beije/neumann/parserxml/andreotti_mongiello/XmlParser.java
+++ Esercizi/src/it/beije/neumann/parserxml/andreotti_mongiello/XmlParser.java
@@ -6,11 +6,16 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class XmlParser {
+	
+	
 	//metodo che legge da un file tutti i caratteri e ritorna una stringBuilder
 	public static StringBuilder fromFileToString(String path) throws IOException {
-
+		
+		//variabile sulla quale memorizzare il contenuto del file 
 		StringBuilder contenuto = new StringBuilder();
 		FileReader fileReader = new FileReader(path);
 		BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -21,5 +26,55 @@ public class XmlParser {
 		}
 		
 		return contenuto;
+	}
+	
+	
+	public static boolean isHeadingTag(String row) {
+//        if( ( (row.charAt(0) == '<') && (row.charAt(1) == '?')) || (row.substring(0, 4).equals("?xml")) )  {
+//            return true;
+//        }else {
+//            return false;
+//        }
+		
+		return ((row.charAt(0) == '<') && (row.charAt(1) == '?')) || (row.substring(0, 4).equals("?xml"));
+    }
+	
+	public static  List<String> listTag( String path ) throws IOException{
+		StringBuilder stringFile = fromFileToString(path);
+		StringBuilder element = new StringBuilder();
+		
+		List<String> listTag = new ArrayList<>();
+		
+		boolean isInsideTag = false;
+		
+		for( int i = 0; i< stringFile.length(); i++ ) {
+			 switch(stringFile.charAt(i)) {
+             case '<':
+                 element.delete(0, element.length() );
+                 isInsideTag = true;
+                 break;
+
+             case '>':
+                 isInsideTag = false;
+                 listTag.add(element.toString());
+                 break;
+
+             default:
+                 if(isInsideTag) element.append(stringFile.charAt(i));
+                 break;
+			 }
+		}
+		
+        if( isHeadingTag(listTag.get(0)) ){
+            listTag.remove(0);
+        }
+        
+	return listTag;	
+	}
+	
+	public static String getRootElement( List<String> listTag ) {
+		String rootElement = listTag.get(0).toString();
+		
+		return rootElement;
 	}
 }
--- Esercizi/src/it/beije/neumann/parserxml/andreotti_mongiello/main.java
+++ Esercizi/src/it/beije/neumann/parserxml/andreotti_mongiello/main.java
@@ -1,6 +1,8 @@
 package it.beije.neumann.parserxml.andreotti_mongiello;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class main {
 
@@ -9,7 +11,14 @@ public class main {
 		
 		try {
 			StringBuilder fileContent = XmlParser.fromFileToString(path);
-			System.out.println(fileContent);
+			//System.out.println(fileContent);
+			List<String> listTag = XmlParser.listTag( path );
+			String rootElement = XmlParser.getRootElement( listTag );
+			System.out.println("Root element: " + rootElement);
+			System.out.println(listTag);
+			
+			
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
