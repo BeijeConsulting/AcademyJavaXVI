4b102df78b6dc8733e3577e8329a5349dc50bf62
MariannaNido <marianna.nido@live.it>
2023-02-13 11:30:19 +0100

    Created Element.java

---
+++ Esercizi/src/it/beije/neumann/parserxml/vellani_nido/Element.java
@@ -0,0 +1,33 @@
+package it.beije.neumann.parserxml.vellani_nido;
+
+public class Element {
+
+	private String name;
+	private String content;
+	
+	public Element() {
+		super();
+	}
+	public Element(String name) {
+		this(name,null);
+	}
+	public Element(String name, String content) {
+		super();
+		this.name = name;
+		this.content = content;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
