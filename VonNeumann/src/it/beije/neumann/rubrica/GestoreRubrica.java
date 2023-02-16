542b4f7b2afc4fb84903863616f30a8e8c63c2d1
Levva01 <vellanifederico13@gmail.com>
2023-02-16 15:22:28 +0100

    + vediListaContatti()

---
+++ VonNeumann/src/it/beije/neumann/rubrica/GestoreRubrica.java
@@ -0,0 +1,139 @@
+package it.beije.neumann.rubrica;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class GestoreRubrica {
+	
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int scelta = 0;
+
+	    do {
+	        System.out.println("1. Vedi lista contatti");
+	        System.out.println("2. Cerca contatto");
+	        System.out.println("3. Inserisci nuovo contatto");
+	        System.out.println("4. Modifica contatto");
+	        System.out.println("5. Cancella contatto");
+	        System.out.println("6. Trova contatti duplicati");
+	        System.out.println("7. Unisci contatti duplicati");
+	        System.out.println("0. Esci");
+
+	        System.out.print("Scegli un'operazione: ");
+	        scelta = scanner.nextInt();
+
+	        switch (scelta) {
+	            case 1:
+	                vediListaContatti();
+	                break;
+	            case 2:
+	                cercaContatto();
+	                break;
+	            case 3:
+	                inserisciNuovoContatto();
+	                break;
+	            case 4:
+	                modificaContatto();
+	                break;
+	            case 5:
+	                cancellaContatto();
+	                break;
+	            case 6:
+	                trovaContattiDuplicati();
+	                break;
+	            case 7:
+	                unisciContattiDuplicati();
+	                break;
+	            case 0:
+	                System.out.println("Grazie per aver usato il gestore di rubrica!");
+	                break;
+	            default:
+	                System.out.println("Scelta non valida. Riprova.");
+	        }
+
+	    } while (scelta != 0);
+
+	    scanner.close();
+
+	}
+	
+	private static void vediListaContatti() {
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet rs = null;
+
+		try {
+		    connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/neumann?serverTimezone=CET&useSSL=false", "root", "root");
+		    statement = connection.prepareStatement("SELECT * FROM contatti ORDER BY nome, cognome");
+		    rs = statement.executeQuery();
+
+		    System.out.println("Elenco contatti:");
+
+		    while (rs.next()) {
+		        String nome = rs.getString("nome");
+		        String cognome = rs.getString("cognome");
+		        String telefono = rs.getString("telefono");
+		        String email = rs.getString("email");
+		        String note = rs.getString("note");
+
+		        System.out.println("---------------------------------------------------------------------");
+		        System.out.println(nome + " " + cognome + ", " + telefono + ", " + email + ", " + note);
+		        System.out.println("---------------------------------------------------------------------");
+		    }
+
+		} catch (SQLException e) {
+		    e.printStackTrace();
+		} finally {
+		    try {
+		        if (rs != null) {
+		            rs.close();
+		        }
+		        if (statement != null) {
+		            statement.close();
+		        }
+		        if (connection != null) {
+		            connection.close();
+		        }
+		    } catch (SQLException e) {
+		        e.printStackTrace();
+		    }
+		}
+	}
+
+	private static void unisciContattiDuplicati() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void trovaContattiDuplicati() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void cancellaContatto() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void modificaContatto() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void inserisciNuovoContatto() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void cercaContatto() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
