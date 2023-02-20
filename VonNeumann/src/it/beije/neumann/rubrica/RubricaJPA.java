7dfb7d021905a5b7dbe9b57f302da494d55c4ba7
ivfly <iv0fly@gmail.com>
2023-02-20 12:28:55 +0100

    JPA

---
+++ VonNeumann/src/it/beije/neumann/rubrica/RubricaJPA.java
@@ -0,0 +1,63 @@
+package it.beije.neumann.rubrica;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class RubricaJPA {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("VonNeumann");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		
+		//INSERT
+		Contatto contatto = new Contatto();
+//		contatto.setId(9);
+//		contatto.setName("Paride");
+//		contatto.setSurname("Gialli");
+//		contatto.setEmail("paride@giallo.it");
+//		
+//		System.out.println("contatto PRE : " + contatto);
+//		entityManager.persist(contatto);
+//		System.out.println("contatto POST : " + contatto);
+
+		//SELECT JPQL
+//		Query query = entityManager.createQuery("SELECT c FROM Contatto as c");
+//		List<Contatto> contatti = query.getResultList();
+////		for (Contatto c : contatti) {
+////			System.out.println(c);
+////		}
+//		System.out.println(contatti);
+		
+		contatto = entityManager.find(Contatto.class, 1);
+		System.out.println(contatto);
+
+		//UPDATE
+//		contatto = contatti.get(contatti.size()-1);
+		//contatto.setId(9);
+//		contatto.setName("Youness");
+//		contatto.setSurname("Verde");
+//		contatto.setEmail("youness@verde.it");
+//
+//		System.out.println("contatto PRE : " + contatto);
+//		entityManager.persist(contatto);
+//		System.out.println("contatto POST : " + contatto);
+
+		//DELETE
+//		entityManager.remove(contatto);
+		
+		transaction.commit();
+		//transaction.rollback();
+
+		entityManager.close();
+	}
+
+}
