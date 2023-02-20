c341152600251b519f2d80d28aa2efe04223d9dd
gianf <gianf@GF>
2023-02-21 00:00:41 +0100

    Gestore rubrica con jpa

--- VonNeumann/src/it/beije/neumann/rubrica/iaria_gestore_rubrica_jpa/GestoreRubricaJPA.java
+++ VonNeumann/src/it/beije/neumann/rubrica/iaria_gestore_rubrica_jpa/GestoreRubricaJPA.java
@@ -1,13 +1,35 @@
 package it.beije.neumann.rubrica.iaria_gestore_rubrica_jpa;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 import javax.persistence.Query;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class GestoreRubricaJPA {
 	
@@ -196,29 +218,40 @@ public class GestoreRubricaJPA {
 			
 			//Faccio inserire all'utente i dati da sostituire
 			System.out.println("Adesso inserisci i nuovi dati: ");
-			System.out.println("Nome: ");
-			s = new Scanner(System.in);
-			contatto.setNome(s.nextLine());
+			Scanner s2 = new Scanner(System.in);
 			
-			System.out.println("Cognome: ");
-			s = new Scanner(System.in);
-			contatto.setCognome(s.nextLine());
+			System.out.print("Nome: ");
+			contattoInput.setNome(s2.nextLine());
+			if(contattoInput.getNome().isEmpty()) {
+				contattoInput.setNome(null);
+			}
 			
-			System.out.println("Email: ");
-			s = new Scanner(System.in);
-			contatto.setEmail(s.nextLine());
+			System.out.print("Cognome: ");
+			contattoInput.setCognome(s2.nextLine());
+			if(contattoInput.getCognome().isEmpty()) {
+				contattoInput.setCognome(null);
+			}
 			
-			System.out.println("Telefono: ");
-			s = new Scanner(System.in);
-			contatto.setTelefono(s.nextLine());
+			System.out.print("Telefono: ");
+			contattoInput.setTelefono(s2.nextLine());
+			if(contattoInput.getTelefono().isEmpty()) {
+				contattoInput.setTelefono(null);
+			}
 			
-			System.out.println("Note: ");
-			s = new Scanner(System.in);
-			contatto.setNote(s.nextLine());
+			System.out.print("Email: ");
+			contattoInput.setEmail(s2.nextLine());
+			if(contattoInput.getEmail().isEmpty()) {
+				contattoInput.setEmail(null);
+			}
+			
+			System.out.print("Note: ");
+			contattoInput.setNote(s2.nextLine());
+			if(contattoInput.getNote().isEmpty()) {
+				contattoInput.setNote(null);
+			}
 			
 			//INSERT --> contattoInput contiene solo il contatto da inserire in un nuovo record
 			entityManager.persist(contattoInput);
-			
 			System.out.println("Contatto modificato correttamente!");
 			System.out.println();
 			
@@ -236,8 +269,409 @@ public class GestoreRubricaJPA {
 		System.out.println();
 	}
 	
+	//Metodo che elimina un contatto
+	public void eliminaContatto(){
+		
+		EntityManagerFactory entityManagerFactory = JPAEntityManager.openEntityManager();
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		
+		Contatti contatto = new Contatti();
+		System.out.println("Contatti: ");
+		vediListaContatti();
+		System.out.println("Inserisci l'id della riga da eliminare: ");
+		Scanner s = new Scanner(System.in);
+		int numeroIdScanner = s.nextInt();
+		System.out.println();
+		
+		contatto = entityManager.find(Contatti.class, numeroIdScanner);
+		if(contatto != null) {
+			entityManager.remove(contatto);
+			System.out.println("Contatto eliminato con successo!");
+		} else {
+			System.out.println("Nessuna corrispondenza");
+		}
 
-	public static void main(String[] args) {
+		transaction.commit();
+		//transaction.rollback();
+
+		entityManager.close();
+		
+		//Torna true se aperta
+		System.out.println(entityManager.isOpen()); //--> false	
+		System.out.println();
+	}
+	
+	//Metodo che trova duplicati
+	public void trovaDuplicati(){
+		int rigaLetta = 0;
+		int rigaLetta2 = 0;
+		int duplicatiTrovati = 0;
+		int count = 0;
+		Contatti contatto = null;
+		String distingui = "";
+		
+		EntityManagerFactory entityManagerFactory = JPAEntityManager.openEntityManager();
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+
+		Query query = entityManager.createQuery("SELECT c FROM Contatti as c");
+		List<Contatti> contatti = query.getResultList();
+		
+		for (Contatti c : contatti) {
+			rigaLetta++;
+			for(Contatti contattiLetti : contatti) {
+				rigaLetta2++;
+				if(rigaLetta != rigaLetta2) {
+					if(contattiLetti.getCognome().contains(c.getCognome()) && contattiLetti.getNome().contains(c.getNome())) {
+						duplicatiTrovati++;
+						System.out.println(contattiLetti.getNome()+" "+contattiLetti.getCognome()+" ha un duplicato alla riga: "+rigaLetta2);
+					}
+				}
+			}
+			System.out.println();
+			rigaLetta2 = 0; 
+			if(duplicatiTrovati != 0) {
+				duplicatiTrovati = 0;
+			}
+		}
+
+		transaction.commit();
+		//transaction.rollback();
+
+		entityManager.close();
+		
+		//Torna true se aperta
+		System.out.println(entityManager.isOpen()); //--> false	
+		System.out.println();
+	}
+	
+	//Metodo che unisce duplicati
+	public void unisciDuplicati(){
+		
+		boolean haDuplicato = false;
+		Contatti contatto = null;
+		List<Contatti> copiaContatto = new ArrayList<>();
+		int riga = 0;
+		
+		EntityManagerFactory entityManagerFactory = JPAEntityManager.openEntityManager();
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+	
+		System.out.println("Contatti: ");
+		vediListaContatti();
+		System.out.println("Inserisci l'id del duplicato da unire: ");
+		Scanner s = new Scanner(System.in);
+		int numeroIdScanner = s.nextInt();
+		System.out.println();
+		
+		//Seleziono contatto con id corrispondente
+		contatto = entityManager.find(Contatti.class, numeroIdScanner);
+		
+		Query query = entityManager.createQuery("SELECT c FROM Contatti as c");
+		List<Contatti> contattiLetti = query.getResultList();
+		for(Contatti c : contattiLetti) { //Se nome e cognome corrispondono e l'id è diverso da quello inserito (così non cancello l'utente originale)
+			if(c.getNome().contains(contatto.getNome()) && c.getCognome().contains(contatto.getCognome()) && c.getId() != contatto.getId()) {
+				haDuplicato = true;
+				contatto.setTelefono(contatto.getTelefono() + " / " + c.getTelefono());
+				entityManager.persist(contatto);
+				entityManager.remove(c);
+			} //Aggiungo i numeri di telefono all'utente originale ed elimino gli altri
+		}
+		
+		System.out.println("Duplicati uniti correttamente!");
+		
+		if(haDuplicato == false) {
+			System.out.println("L'utente non ha duplicati");
+		}
+		
+		transaction.commit();
+		//transaction.rollback();
+
+		entityManager.close();
+		
+		//Torna true se aperta
+		System.out.println(entityManager.isOpen()); //--> false	
+		System.out.println();
+	}
+	
+	//Metodo che esporta dati db su csv
+	public void writeRubricaCSV(String pathFile, String separator) throws IOException{
+		
+		EntityManagerFactory entityManagerFactory = JPAEntityManager.openEntityManager();
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		
+		File rubrica = new File(pathFile);
+		boolean exists = rubrica.exists(); //File esiste
+		FileWriter fileWriter = new FileWriter(pathFile, true);
+		int righeInserite = 0;
+			
+		if(!exists) {//Creo la prima riga con NOME-COGNOME-NOTE-TELEFONO se il file non esiste
+			fileWriter.write("ID" + separator);
+			fileWriter.write("COGNOME" + separator);
+			fileWriter.write("NOME" + separator);
+			fileWriter.write("TELEFONO" + separator);
+			fileWriter.write("EMAIL" + separator);
+			fileWriter.write("NOTE" + "\n");
+		}
+		
+		//Prendo i dati dal db
+		Query query = entityManager.createQuery("SELECT c FROM Contatti as c");
+		List<Contatti> contatti = query.getResultList();
+		for (Contatti c : contatti) {
+			righeInserite++;
+			fileWriter.write(c.getId() + separator);
+			fileWriter.write(c.getCognome() + separator);
+			fileWriter.write(c.getNome() + separator);	
+			fileWriter.write(c.getTelefono() + separator);
+			fileWriter.write(c.getEmail() + separator);
+			fileWriter.write(c.getNote() + "\n");
+		}
+		System.out.println("Sono state inserite "+righeInserite+" righe");
+
+		fileWriter.close(); //Chiudo il file e salvo le modifiche
+		
+		transaction.commit();
+		//transaction.rollback();
+
+		entityManager.close();
+		
+		//Torna true se aperta
+		System.out.println(entityManager.isOpen()); //--> false	
+		System.out.println();
+			
+	}
+	
+	//Metodo che importa il file csv su db
+	public void importaContattiCSV(String pathFile, String separator) throws FileNotFoundException, IOException{
+		
+		EntityManagerFactory entityManagerFactory = JPAEntityManager.openEntityManager();
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		
+		int righeInserite = 0;
+		String[] fields = null;
+		Contatti contatto = null;
+		String r = null;
+		
+		FileReader fileReader = new FileReader(pathFile);
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		
+		//Prendo i dati dal db
+		Query query = entityManager.createQuery("SELECT c FROM Contatti as c");
+		List<Contatti> contatti = query.getResultList();
+			
+			while (bufferedReader.ready()) {
+				r = bufferedReader.readLine();
+				fields = r.split(separator);
+				
+				righeInserite++;
+				contatto = new Contatti();
+				
+				contatto.setCognome(fields[1]);
+				contatto.setNome(fields[2]);
+				contatto.setTelefono(fields[3]);
+				contatto.setEmail(fields[4]);
+				contatto.setNote(fields[5]);
+				
+				//Se si trova alla seconda riga (quindi salto NOME;COGNOME;TELEFONO;EMAIL;NOTE)
+				if(righeInserite > 1) {
+					entityManager.persist(contatto);
+				}
+				
+				//session.save("UPDATE contatti set nome = case when nome in('null','') then null else nome end, cognome = case when cognome in('null','') then null else cognome end, telefono = case when telefono in('null','') then null else telefono end, email = case when email in('null','') then null else email end, note = case when note in('null','') then null else note end");
+
+			}
+			
+			//Salto NOME;COGNOME;TELEFONO;EMAIL;NOTE togliendo 1 da righeInserite che contiene il numero di cicli effettuati
+			System.out.println("Sono state inserite "+ (righeInserite-1) +" righe");
+			System.out.println();
+			
+			bufferedReader.close();
+			
+			transaction.commit();
+			//transaction.rollback();
+
+			entityManager.close();
+			
+			//Torna true se aperta
+			System.out.println(entityManager.isOpen()); //--> false	
+			System.out.println();
+
+	}
+	
+	//Metodo che esporta dati db su xml
+	public void writeRubricaXML(String pathFile) throws ParserConfigurationException, IOException, SAXException, ClassNotFoundException, DOMException, SQLException, TransformerException{
+		
+		EntityManagerFactory entityManagerFactory = JPAEntityManager.openEntityManager();
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		
+		int righeInserite = 0;
+			
+		//Creo file
+		File rubricaXml = new File(pathFile);
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		
+		//Costruiamo una factory per processare il nostro flusso di dati
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		Document doc = null;
+		
+		//Creo il root che una volta creato il file diventerà rubrica
+		Element root = null;
+		
+		//Se il file esiste, analizza e verifica il root per poi effettuare l'append
+		if(rubricaXml.exists()) {
+			doc = docBuilder.parse(pathFile);
+			root = doc.getDocumentElement();			
+		} else { //Se non esiste crea un nuovo document con il root "Rubrica"
+			doc = docBuilder.newDocument();
+			root = doc.createElement("Rubrica");
+			doc.appendChild(root);
+		}
+		
+		//Prendo i dati dal db
+		Query query = entityManager.createQuery("SELECT c FROM Contatti as c");
+		List<Contatti> contatti = query.getResultList();
+		for (Contatti c : contatti) {
+			righeInserite++;
+			
+			//Per ogni contatto inserisci: Contatto ed i figli nome,cognome ecc. con valori
+			Element contatto = doc.createElement("Contatto");
+			Element id = doc.createElement("ID");
+			id.setTextContent(Integer.toString(c.getId()));
+			Element cognome = doc.createElement("Cognome");
+			cognome.setTextContent(c.getCognome());
+			Element nome = doc.createElement("Nome");
+			nome.setTextContent(c.getNome());
+			Element telefono = doc.createElement("Telefono");
+			telefono.setTextContent(c.getTelefono());
+			Element email = doc.createElement("Email");
+			email.setTextContent(c.getEmail());
+			Element note = doc.createElement("Note");
+			note.setTextContent(c.getNote());	
+			
+			//Inserisco contatto come figlio di root (Rubrica) e poi aggiungo ai contatti i valori
+			root.appendChild(contatto);
+			contatto.appendChild(id);
+			contatto.appendChild(cognome);
+			contatto.appendChild(nome);
+			contatto.appendChild(telefono);
+			contatto.appendChild(email);
+			contatto.appendChild(note);
+		}
+		System.out.println("Sono state inserite "+righeInserite+" righe");
+		System.out.println();
+		
+		//Scrivo il contenuto nel file xml
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = transformerFactory.newTransformer();
+		DOMSource source = new DOMSource(doc);
+		StreamResult result = new StreamResult(new File(pathFile));
+		transformer.transform(source, result);
+		
+		transaction.commit();
+		//transaction.rollback();
+
+		entityManager.close();
+		
+		//Torna true se aperta
+		System.out.println(entityManager.isOpen()); //--> false	
+		System.out.println();
+		
+	}
+	
+	//Per leggere dati .XML
+	public static List<Element> getChildElements(Element e) {
+		List<Element> elements = new ArrayList<Element>();
+		NodeList childNodes = e.getChildNodes();
+		for (int i = 0; i < childNodes.getLength(); i ++) {
+			Node node = childNodes.item(i);
+			if (node instanceof Element) {
+				elements.add((Element) node);
+			}
+		}
+		
+		return elements;
+	}
+	
+	//Metodo che importa il file xml su db
+	public void importaContattiXML(String pathFile) throws ParserConfigurationException, IOException, SAXException, ClassNotFoundException{
+		
+		EntityManagerFactory entityManagerFactory = JPAEntityManager.openEntityManager();
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+
+		int righeInserite = 0;
+		
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		Document document = documentBuilder.parse(pathFile);		
+		Element rootElement = document.getDocumentElement();
+		List<Element> elements = getChildElements(rootElement);
+			
+		Contatti contatto = null;
+		
+		for (Element el : elements) {
+			righeInserite++;
+			contatto = new Contatti();
+			
+			List<Element> values = getChildElements(el);
+
+			//Leggo in verticale
+			for (Element v : values) {
+				String valore = v.getNodeName().toLowerCase(); //Prendo il nome del nodo in minuscolo per confrontarlo
+
+				switch (valore) {
+				case "cognome":
+					contatto.setCognome(v.getTextContent());
+					break;
+				case "nome":
+					contatto.setNome(v.getTextContent());
+					break;
+				case "telefono":
+					contatto.setTelefono(v.getTextContent());
+					break;
+				case "email":
+					contatto.setEmail(v.getTextContent());
+					break;
+				case "note":
+					contatto.setNote(v.getTextContent());
+					break;
+				}
+			}
+			
+			//INSERT
+			entityManager.persist(contatto);
+	
+		}
+			
+			//UPDATE
+			//("UPDATE contatti set nome = case when nome in('null','') then null else nome end, cognome = case when cognome in('null','') then null else cognome end, telefono = case when telefono in('null','') then null else telefono end, email = case when email in('null','') then null else email end, note = case when note in('null','') then null else note end");
+			
+			System.out.println("Sono state inserite "+righeInserite+" righe");
+			System.out.println();
+			
+			transaction.commit();
+			//transaction.rollback();
+
+			entityManager.close();
+			
+			//Torna true se aperta
+			System.out.println(entityManager.isOpen()); //--> false	
+			System.out.println();
+
+	}
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException, DOMException, ParserConfigurationException, SAXException, SQLException, TransformerException {
 		String pathForCSV = "/Users/gianf/Desktop/contatti.csv";
 		String pathForXml = "/Users/gianf/Desktop/rubrica.xml";
 		String separator = "-";
@@ -291,36 +725,36 @@ public class GestoreRubricaJPA {
 					gestoreRubricaJPA.main(args); //Richiama il programma così si può continuare ad usare, dato che l'utente non ha ancora fatto l'exit
 					break;
 				case 5:
-//					gestoreRubricaJPA.eliminaContatto();
+					gestoreRubricaJPA.eliminaContatto();
 					System.out.println("Lista aggiornata");
-//					gestoreRubricaJPA.vediListaContatti();
+					gestoreRubricaJPA.vediListaContatti();
 					gestoreRubricaJPA.main(args);
 					break;
 				case 6:
-//					gestoreRubricaJPA.trovaDuplicati();
+					gestoreRubricaJPA.trovaDuplicati();
 					gestoreRubricaJPA.main(args);
 					break;
 				case 7:
-//					gestoreRubricaJPA.unisciDuplicati();
+					gestoreRubricaJPA.unisciDuplicati();
 					System.out.println();
 					System.out.println("Lista aggiornata");
-//					ggestoreRubricaJPA.vediListaContatti();
+					gestoreRubricaJPA.vediListaContatti();
 					gestoreRubricaJPA.main(args);
 					break;
 				case 8:
-//					gestoreRubricaJPA.writeRubricaCSV(pathForCSV,separator);
+					gestoreRubricaJPA.writeRubricaCSV(pathForCSV,separator);
 					gestoreRubricaJPA.main(args);
 					break;
 				case 9:
-//					gestoreRubricaJPA.importaContattiCSV(pathForCSV,separator);
+					gestoreRubricaJPA.importaContattiCSV(pathForCSV,separator);
 					gestoreRubricaJPA.main(args);
 					break;
 				case 10:
-//					gestoreRubricaJPA.writeRubricaXML(pathForXml);
+					gestoreRubricaJPA.writeRubricaXML(pathForXml);
 					gestoreRubricaJPA.main(args);
 					break;
 				case 11:
-//					gestoreRubricaJPA.importaContattiXML(pathForXml);
+					gestoreRubricaJPA.importaContattiXML(pathForXml);
 					gestoreRubricaJPA.main(args);
 					break;
 				case 12:
