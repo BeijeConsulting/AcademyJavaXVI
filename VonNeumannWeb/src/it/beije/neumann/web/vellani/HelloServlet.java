7bfdfa9f35eda332e529cffc31098f2d652bf0ab
Levva01 <vellanifederico13@gmail.com>
2023-02-22 17:57:14 +0100

    + mapping servlet fixed

---
+++ VonNeumannWeb/src/it/beije/neumann/web/vellani/HelloServlet.java
@@ -0,0 +1,29 @@
+package it.beije.neumann.web.vellani;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/vellani/hello")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		LocalDateTime dateTime = LocalDateTime.now();
+		
+		response.getWriter().append("<html><body><p>").append("ORA ESATTA: <br/>").append(dateTime.toString()).append("</p></body></html>");
+	}
+
+}
