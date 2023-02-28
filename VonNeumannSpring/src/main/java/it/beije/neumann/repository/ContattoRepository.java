4a51032bd640f0bd85e7256b5fbb4073929f39dc
ivfly <iv0fly@gmail.com>
2023-02-28 16:30:55 +0100

    JpaRepository

---
+++ VonNeumannSpring/src/main/java/it/beije/neumann/repository/ContattoRepository.java
@@ -0,0 +1,18 @@
+package it.beije.neumann.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import it.beije.neumann.model.Contatto;
+
+
+@Repository
+public interface ContattoRepository extends JpaRepository<Contatto, Integer> {
+
+	public List<Contatto> findBySurname(String surname);
+	
+	public List<Contatto> findBySurnameAndName(String surname, String name);
+	
+}
