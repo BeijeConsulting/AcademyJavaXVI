bad50a507d134d86bf652bed5c6958760d96e9ee
nverzaschi <nverzaschi@gmail.com>
2023-03-06 11:43:10 +0100

    fix persistence and Orders

--- VonNeumannSpring/src/main/java/it/beije/neumann/ecommerce_shoes/model/Orders.java
+++ VonNeumannSpring/src/main/java/it/beije/neumann/ecommerce_shoes/model/Orders.java
@@ -16,7 +16,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import it.beije.neumann.model.OrderItem;
 
 @Entity
 @Table(name = "orders")
