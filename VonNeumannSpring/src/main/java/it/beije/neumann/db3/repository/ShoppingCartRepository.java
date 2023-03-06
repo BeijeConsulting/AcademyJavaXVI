0dc37eb9ae13c773097ec3e7a60b13fbe671e5ee
gianf <gianf@GF>
2023-03-06 10:51:20 +0100

    shoppingcart e shoppingcartitem

---
+++ VonNeumannSpring/src/main/java/it/beije/neumann/db3/repository/ShoppingCartRepository.java
@@ -0,0 +1,13 @@
+package it.beije.neumann.db3.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import it.beije.neumann.db3.model.ShoppingCart;
+
+@Repository
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Integer>{
+
+}
