145fb4a852f77de1c16e72cd6536fe4216db582a
Davide Vanoli <davidevanoli2@gmail.com>
2023-03-06 10:41:28 +0100

    merge con modifiche in user repo

--- VonNeumannSpring/src/main/java/it/beije/neumann/ecommerce_shoes/repository/UserRepository.java
+++ VonNeumannSpring/src/main/java/it/beije/neumann/ecommerce_shoes/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package it.beije.neumann.ecommerce_shoes.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import it.beije.neumann.ecommerce_shoes.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
-
+	public List<User> findByCredentials(String email, String password);
 }
