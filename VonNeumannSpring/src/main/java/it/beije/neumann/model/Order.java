192c354437a0158d96e1eabc4dcd3031106c3c06
Levva01 <vellanifederico13@gmail.com>
2023-03-06 10:06:24 +0100

    + fix models

--- VonNeumannSpring/src/main/java/it/beije/neumann/model/Order.java
+++ VonNeumannSpring/src/main/java/it/beije/neumann/model/Order.java
@@ -1,6 +1,7 @@
 package it.beije.neumann.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -10,6 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -18,94 +20,125 @@ import javax.persistence.Table;
 @Table(name = "orders")
 public class Order {
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name = "id")
-	private Integer id;
-	
-	@Column(name = "users_id")
-	private Integer userId;
-	
-	@Column(name = "datetime")
-	private LocalDateTime datetime;
-	
-	@Column(name = "amount")
-	private Double amount;
-	
-	@Column(name = "discount")
-	private Double discount;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-//	@OneToMany(targetEntity = OrderItem.class, fetch = FetchType.LAZY)
-	@OneToMany(targetEntity = OrderItem.class, fetch = FetchType.EAGER)
-	@JoinColumn(name = "orders_id")
-	private List<OrderItem> items;
-	
-	
-	public Integer getId() {
+    @Column(name = "transaction")
+    private String transaction;
+
+    @Column(name = "transaction_date")
+    private LocalDateTime transactionDate;
+
+    @Column(name = "payment_status")
+    private String paymentStatus;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "total_price")
+    private Double totalPrice;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "disabled_at")
+    private LocalDateTime disabledAt;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
-	
-	public Integer getUserId() {
-		return userId;
+	public String getTransaction() {
+		return transaction;
 	}
 
-	public void setUserId(Integer userId) {
-		this.userId = userId;
+	public void setTransaction(String transaction) {
+		this.transaction = transaction;
 	}
 
-	
-	public LocalDateTime getDatetime() {
-		return datetime;
+	public LocalDateTime getTransactionDate() {
+		return transactionDate;
 	}
 
-	public void setDatetime(LocalDateTime datetime) {
-		this.datetime = datetime;
+	public void setTransactionDate(LocalDateTime transactionDate) {
+		this.transactionDate = transactionDate;
 	}
 
-	
-	public Double getAmount() {
-		return amount;
+	public String getPaymentStatus() {
+		return paymentStatus;
 	}
 
-	public void setAmount(Double amount) {
-		this.amount = amount;
+	public void setPaymentStatus(String paymentStatus) {
+		this.paymentStatus = paymentStatus;
 	}
 
-	
-	public Double getDiscount() {
-		return discount;
+	public String getStatus() {
+		return status;
 	}
 
-	public void setDiscount(Double discount) {
-		this.discount = discount;
+	public void setStatus(String status) {
+		this.status = status;
 	}
 
-	
-	public List<OrderItem> getItems() {
-		return items;
+	public Double getTotalPrice() {
+		return totalPrice;
 	}
 
-	public void setItems(List<OrderItem> items) {
-		this.items = items;
+	public void setTotalPrice(Double totalPrice) {
+		this.totalPrice = totalPrice;
 	}
 
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
 
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getDisabledAt() {
+		return disabledAt;
+	}
+
+	public void setDisabledAt(LocalDateTime disabledAt) {
+		this.disabledAt = disabledAt;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public List<OrderItem> getOrderItems() {
+		return orderItems;
+	}
+
+	public void setOrderItems(List<OrderItem> orderItems) {
+		this.orderItems = orderItems;
+	}
+
+	@Override
 	public String toString() {
-		StringBuilder builder = new StringBuilder("{")
-				.append(" id: ").append(id)
-				.append(", userId: ").append(userId)
-				.append(", datetime: ").append(datetime)
-				.append(", amount: ").append(amount)
-				.append(", discount: ").append(discount)
-				.append(", items: ").append(items)
-				.append("}");
-
-		return builder.toString();
+		return "Order [id=" + id + ", transaction=" + transaction + ", transactionDate=" + transactionDate
+				+ ", paymentStatus=" + paymentStatus + ", status=" + status + ", totalPrice=" + totalPrice
+				+ ", createdAt=" + createdAt + ", disabledAt=" + disabledAt + ", user=" + user + ", orderItems="
+				+ orderItems + "]";
 	}
 
+    
 }
--- VonNeumannSpring/src/main/java/it/beije/neumann/model/ShoppingCartItem.java
+++ VonNeumannSpring/src/main/java/it/beije/neumann/model/ShoppingCartItem.java
@@ -1,76 +1,93 @@
 package it.beije.neumann.model;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "shopping_cart")
-public class ShoppingCart {
+@Table(name = shopping_cart_item)
+public class ShoppingCartItem {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name = "id")
-	private Integer id;
-	
-	@Column(name = "created_at")
-	private LocalDateTime createdAt;
-	
-	@Column(name = "disabled_at")
-	private LocalDateTime disabledAt;
-	
-	@Column(name = "user_id")
-	private Integer userId;
-	
-	@OneToMany(fetch = FetchType.EAGER)
-	private List<ShoppingCartItem> shoppingCartItems;
-	
-	public Integer getId() {
-		return id;
-	}
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = id)
+    private Integer id;
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    @Column(name = quantity)
+    private Integer quantity;
 
-	public LocalDateTime getCreatedAt() {
-		return createdAt;
-	}
+    @Column(name = created_at)
+    private LocalDateTime createdAt;
 
-	public void setCreatedAt(LocalDateTime createdAt) {
-		this.createdAt = createdAt;
-	}
+    @Column(name = disabled_at)
+    private LocalDateTime disabledAt;
 
-	public LocalDateTime getDisabledAt() {
-		return disabledAt;
-	}
+    @ManyToOne
+    @JoinColumn(name = shopping_cart_id)
+    private ShoppingCart shoppingCart;
 
-	public void setDisabledAt(LocalDateTime disabledAt) {
-		this.disabledAt = disabledAt;
-	}
+    @ManyToOne
+    @JoinColumn(name = product_details_id)
+    private ProductDetails productDetails;
 
-	public Integer getUserId() {
-		return userId;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setUserId(Integer userId) {
-		this.userId = userId;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public List<ShoppingCartItem> getShoppingCartItems() {
-		return shoppingCartItems;
-	}
+    public Integer getQuantity() {
+        return quantity;
+    }
 
-	public void setCartItems(List<ShoppingCartItem> shoppingCartItems) {
-		this.shoppingCartItems = shoppingCartItems;
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getDisabledAt() {
+        return disabledAt;
+    }
+
+    public void setDisabledAt(LocalDateTime disabledAt) {
+        this.disabledAt = disabledAt;
+    }
+
+    public ShoppingCart getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public void setShoppingCart(ShoppingCart shoppingCart) {
+        this.shoppingCart = shoppingCart;
+    }
+
+    public ProductDetails getProductDetails() {
+        return productDetails;
+    }
+
+    public void setProductDetails(ProductDetails productDetails) {
+        this.productDetails = productDetails;
+    }
+    
+    @Override
+	public String toString() {
+		return ShoppingCartItem [id= + id + , quantity= + quantity + , createdAt= + createdAt + , disabledAt=
+				+ disabledAt + , shoppingCart= + shoppingCart + , productDetails= + productDetails + ];
 	}
-	
 }
