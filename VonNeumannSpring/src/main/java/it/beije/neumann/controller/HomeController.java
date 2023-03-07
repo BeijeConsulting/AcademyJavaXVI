e22a67183e31002e87f66dc87de98a4ef6c884d5
Levva01 <vellanifederico13@gmail.com>
2023-03-07 16:59:53 +0100

    ...

---
+++ VonNeumannSpring/src/main/java/it/beije/neumann/controller/HomeController.java
@@ -0,0 +1,36 @@
+package it.beije.neumann.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class HomeController {
+    
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String home(Model model) {
+    	return "home";
+    }
+    
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Model model) {
+    	return "login";
+    }
+
+    @RequestMapping(value = "/create_account", method = RequestMethod.GET)
+    public String createAccount(Model model) {
+    	return "create_account";
+    }
+    
+    @RequestMapping(value = "/dettagli_prodotto", method = RequestMethod.GET)
+    public String dettagliProdotto(Model model) {
+    	return "dettagli_prodotto";
+    }
+    
+    
+
+
+
+}
