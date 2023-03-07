a44ca4cd3073fe0a33ab3a695833ac7b3965eefc
francescoandreotti <francescoandreotti00@gmail.com>
2023-03-07 16:53:06 +0100

    Added delete cart  item controller

---
+++ VonNeumannSpring/src/main/java/it/beije/neumann/ecommerce_shoes/controller/DeleteCartItemController.java
@@ -0,0 +1,41 @@
+package it.beije.neumann.ecommerce_shoes.controller;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import it.beije.neumann.ecommerce_shoes.model.ShoppingCartItem;
+import it.beije.neumann.ecommerce_shoes.repository.ShoppingCartItemRepository;
+
+@Controller
+public class DeleteCartItemController {
+
+	@Autowired
+	@Qualifier("shoppingCartItemRepository")
+	private ShoppingCartItemRepository shoppingCartItemRepository;
+	
+	@RequestMapping(value = "/delete", method = RequestMethod.GET)
+	public String getLogin(@RequestParam("id") String id, Model model) throws IOException {
+		System.out.println("GET /details, id: " + id);
+		int intId = 0;
+		try{
+            intId = Integer.parseInt(id);
+        }
+        catch (NumberFormatException ex){
+            ex.printStackTrace();
+        }
+		
+		Optional<ShoppingCartItem> item = shoppingCartItemRepository.findById(intId);
+		ShoppingCartItem i = item.get();
+		i.setDisabledAt(LocalDateTime.now());
+		return "shopping_cart";
+	}
+}
