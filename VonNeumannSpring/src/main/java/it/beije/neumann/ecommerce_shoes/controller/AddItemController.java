1ddbd356d0fdea1b17b23571a03bf46a8fd3811c
Davide Vanoli <davidevanoli2@gmail.com>
2023-03-07 12:33:07 +0100

    modifiche addItem

--- VonNeumannSpring/src/main/java/it/beije/neumann/ecommerce_shoes/controller/AddItemController.java
+++ VonNeumannSpring/src/main/java/it/beije/neumann/ecommerce_shoes/controller/AddItemController.java
@@ -15,30 +15,43 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import it.beije.neumann.ecommerce_shoes.model.Product;
+import it.beije.neumann.ecommerce_shoes.model.ProductDetails;
+import it.beije.neumann.ecommerce_shoes.model.ShoppingCart;
 import it.beije.neumann.ecommerce_shoes.model.ShoppingCartItem;
+import it.beije.neumann.ecommerce_shoes.model.User;
 import it.beije.neumann.ecommerce_shoes.repository.ProductDetailsRepository;
+import it.beije.neumann.ecommerce_shoes.repository.ShoppingCartRepository;
 
 
 @Controller
 public class AddItemController {
-
-	/*
+	@RequestMapping(value = "/addItem", method = RequestMethod.GET)
+	public String test() {
+		return "index";
+	}
+/*
 	@Autowired
 	@Qualifier("productDetailsRepository")
 	private ProductDetailsRepository productDetailsRepository;
-	*/
+	
+	@Autowired
+	@Qualifier("shoppingCartRepository")
+	private ShoppingCartRepository shoppingCartRepository;
 	
 	@RequestMapping(value = "/addItem", method = RequestMethod.POST)
 	public String addItem(Model model, HttpServletRequest request,
-						  @RequestParam(required = true) String size,
+						  @RequestParam(required = true) String productDetailsId,
 						  @RequestParam(required = true) String quantity) throws IOException {
 		System.out.println("POST /addItem");
-		HttpSession session = request.getSession();
-		Product product = (Product)session.getAttribute("product");
+		HttpSession session = request.getSession();		
+		User user = (User)session.getAttribute("user");
 		
+		ShoppingCart cart = shoppingCartRepository.findByUserId(user.getId());
 		ShoppingCartItem cartItem = new ShoppingCartItem();
 		
-		//productDetailsRepository.find
+		ProductDetails prodDetails = productDetailsRepository.findById(Integer.parseInt(productDetailsId)).get();
+		
 		return "index";
 	}
+	*/
 }
