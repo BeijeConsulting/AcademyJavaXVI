26dbd8a01afe343e2c97808e5e4babcf7e08ea6b
Davide Vanoli <davidevanoli2@gmail.com>
2023-03-08 10:27:50 +0100

    work in progress

--- VonNeumannSpring/src/main/java/it/beije/neumann/ecommerce_shoes/repository/AddressesRepository.java
+++ VonNeumannSpring/src/main/java/it/beije/neumann/ecommerce_shoes/repository/AddressesRepository.java
@@ -1,11 +1,17 @@
 package it.beije.neumann.ecommerce_shoes.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import it.beije.neumann.ecommerce_shoes.model.Addresses;
+import it.beije.neumann.ecommerce_shoes.model.User;
 
 
 @Repository
 public interface AddressesRepository extends JpaRepository<Addresses, Integer>{
-
+	@Query(value = "SELECT address.id FROM addresses as address WHERE address.user_id = :userId", nativeQuery = true)
+	public List<Integer> findByUserId(@Param(value = "userId") Integer userId);
 }
