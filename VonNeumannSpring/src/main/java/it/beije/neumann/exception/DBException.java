7be49c9a3d8adbd2970591b237650545f7da2451
ivfly <iv0fly@gmail.com>
2023-03-14 10:54:36 +0100

    + CustomExceptionHandler

---
+++ VonNeumannSpring/src/main/java/it/beije/neumann/exception/DBException.java
@@ -0,0 +1,9 @@
+package it.beije.neumann.exception;
+
+public class DBException extends NeumannException {
+	private static final long serialVersionUID = 4364777109633190965L;
+
+	public DBException(String message) {
+        super(message);
+    }
+}
---
+++ VonNeumannSpring/src/main/java/it/beije/neumann/exception/NeumannException.java
@@ -0,0 +1,11 @@
+package it.beije.neumann.exception;
+
+public class NeumannException extends RuntimeException {
+
+	private static final long serialVersionUID = 2480282740891918688L;
+
+	public NeumannException(String message) {
+		super(message);
+	}
+
+}
