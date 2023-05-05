52f56b4cacb808f5c1ed90e8c8fdd3ae07d8b34e
youn <mm@00>
2023-05-05 14:59:49 +0200

    vedi se funziona

---
+++ Mockito/src/test/java/it/beije/mockito/MockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.beije.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
