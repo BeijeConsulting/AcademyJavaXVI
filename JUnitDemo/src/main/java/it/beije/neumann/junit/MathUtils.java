ba44b2a327ce09eb9ebd6a43d11a26229b51a224
bruceu <bruceu@zulul.alienPls>
2023-05-08 17:33:17 +0200

    demo

--- JUnitDemo/src/main/java/it/beije/neumann/junit/MathUtils.java
+++ JUnitDemo/src/main/java/it/beije/neumann/junit/MathUtils.java
@@ -2,10 +2,39 @@ package it.beije.neumann.junit;
 
 public class MathUtils {
 	
-	
-	public static int subtraction (int a, int b) {
-		
+	public static int subtraction (int a, int b) {		
 		return a - b;
 	}
+	
+	public static int[] arraySum(int[] a, int[] b) {
+		int maxL = Math.max(a.length, b.length);
+		int[] c = new int[maxL];
+		for (int i = 0; i < maxL; i++) {
+			int valA = 0;
+			int valB = 0;
+			if (i < a.length) {
+				valA = a[i];
+			}
+			if (i < b.length) {
+				valB = b[i];
+			}
+			c[i] = valA + valB;
+		}
+		return c;
+	}
+	
+	public static String sumExpression(int[] numbers) {
+		if (numbers == null) {
+			return null;
+		}
+		StringBuilder result = new StringBuilder();
+		for(int i = 0; i < numbers.length; i++) {
+			result.append(numbers[i] + " + ");
+		}
+		return result.toString();
+	}
 
+	public static boolean isEven(int n) {
+		return n % 2 == 0;
+	}
 }
--- JUnitDemo/src/test/java/it/beije/neumann/junit/MathUtilsTest.java
+++ JUnitDemo/src/test/java/it/beije/neumann/junit/MathUtilsTest.java
@@ -1,14 +1,24 @@
 package it.beije.neumann.junit;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
-
+	
+	// --- assertEquals, assertNotEquals ---
 	@Test
-	void test() {
+	void testSubtraction1() {
 		int expected = -9;
 		int actual;
 		
@@ -16,10 +26,94 @@ class MathUtilsTest {
 		
 		assertEquals(expected, actual);
 	}
+	
+	@Test
+	void testSubtraction2() {
+		int expected = 50;
+		int actual;
+		
+		actual = MathUtils.subtraction(2, 1);
+		
+		assertNotEquals(expected, actual);
+	}
+	
+	// --- assertArrayEquals ---	
+	@Test
+	void testArraySum1() {
+		int a[] = {3, 6, 8};
+		int b[] = {5, 9, 4};
+		
+		int expected[] = {8, 15, 12};
+		int actual[] = MathUtils.arraySum(a, b);
+		
+		assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	void testArraySum2() {
+		int a[] = {3, 6, 8, 9, 4};
+		int b[] = {5, 9, 4};
+		
+		int expected[] = {8, 15, 12, 9, 4};
+		int actual[] = MathUtils.arraySum(a, b);
+		
+		assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	void testArraySum3() {
+		int a[] = {3, 6, 8};
+		int b[] = {5, 9, 4, 7, 3};
+		
+		int expected[] = {8, 15, 12};
+		int actual[] = MathUtils.arraySum(a, b);
+		
+		assertArrayEquals(expected, actual);
+	}
+	
+	// --- assertNull ---	
+	@Test
+	void testSumExpression1() {
+		assertNull(MathUtils.sumExpression(null));
+	}
+	
+	@Test
+	void testSumExpression2() {
+		int a[] = {3, 6, 8};
+		assertNotNull(MathUtils.sumExpression(a));
+	}
+	
+	// --- assertTrue, assertFalse ---	
+	@Test
+	void testIsEven1() {
+		assertTrue(MathUtils.isEven(6));
+	}
+	
+	@Test
+	void testIsEven2() {
+		assertFalse(MathUtils.isEven(5));
+	}
+	
+	@Test
+	void fallito() {
+		System.out.println("Sto eseguendo un test");
+		fail();
+	}
 
 	@Disabled
 	@Test
-	void test2() {
+	void disabledTest() {
 		System.out.println("secondo test");
+	}
+	
+	//le classi con annotations @BeforeAll e @AfterAll devono essere static!
+	@BeforeAll
+	static void beforeTests() {
+		System.out.println("Prima di eseguire i test");
+	}
+	
+	@AfterAll
+	static void afterTests() {
+		System.out.println("Dopo aver eseguito i test");
 	}
 }
