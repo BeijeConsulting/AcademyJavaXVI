3ad9939d8a4aaa8628ed5b4339eb18628ea59ee1
Levva01 <vellanifederico13@gmail.com>
2023-05-09 14:54:31 +0200

    + Esercizi Stream

--- Stream/esercizioRistorante/TestRistorante.java
+++ Stream/esercizioRistorante/TestRistorante.java
@@ -1,14 +1,10 @@
 package esercizioRistorante;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import demo.Person;
 
 public class TestRistorante {
 
@@ -25,33 +21,66 @@ public class TestRistorante {
 		/**
 		 * 1)Scrivere un metodo che stampi una riga per ogni ristorante con nome e numero coperti,
 		 *   in ordine decrescente di numero di coperti.
-		 *   
-		 * 2)Data la lista di ristoranti fornita sopra,scrivere un metodo che restituisca l'insieme
-		 *   dei ristoranti che hanno almeno 45 coperti.
-		 * 
-		 * 3)Data la lista dei ristoranti, scrivere un  metodo che restituisca una mappa tipo ristorante (chiave)
-		 *   lista dei ristoranti di quel tipo(valore).
-		 *   
-		 * 4)Come nell'esercizio precedente, ma con ciascuna lista di ristoranti ordinata per nuemro di coperti.
-		 *
-		 * 5)Data la lista dei ristoranti, scrivere un metodo che stampi i nomi dei ristoranti in ordine 
-		 *   alfabetico separati da virgola.
-		 *   
-		 * 6)Data la lista dei ristoranti,scrivere un metodo che restituisca la somma totale di tutti i coperti
-		 *   dei ristorsnti della lista.
-		 *   
-		 * 7)Data la lista dei ristoranti,scrivere un metodo che restituisca il massimo, il minimo e la media 
-		 *   dei coperti.
-		 * 
-		 * 8)Data la lista dei ristoranti, stampare la lista dei ristoranti con nome che inizia con D e
-		 *   lunghi almeno 7 lettere.
-		 *   
-		 * 9)Calcolare la somma del doppio dei valori pari dei coperti dei ristorsnti della lista.
-		 * 
-		 * 10)Il file Ristorante.csv indica il nome e il numero di coperti di alcuni ristoranti. Dato questo file
-		 *    stampare per ciascuna riga "nome:numero_coperti".
-		 *    Ad esempio, per la prima riga -> "Zi Pietro:45".
 		 */
+		
+		risto.stream().sorted(Comparator.comparing(Ristorante::getCoperti).reversed())
+		.forEach(r -> System.out.println(r.getNome() + ", Coperti: " + r.getCoperti()));
+		
+		 /* 2)Data la lista di ristoranti fornita sopra,scrivere un metodo che restituisca l'insieme
+		 *   dei ristoranti che hanno almeno 45 coperti.
+		 */
+		
+		System.out.println();
+		risto.stream().filter(r -> r.getCoperti() > 45)
+		.forEach(r -> System.out.println(r.getNome() + ", Coperti: " + r.getCoperti()));
+		
+		 /* 3)Data la lista dei ristoranti, scrivere un  metodo che restituisca una mappa tipo ristorante (chiave)
+		 *   lista dei ristoranti di quel tipo(valore).
+		 */
+		System.out.println();
+		System.out.println(risto.stream().collect(Collectors.groupingBy(Ristorante::getTipo)));
+	
+		 /* 4)Come nell'esercizio precedente, ma con ciascuna lista di ristoranti ordinata per nuemro di coperti.
+		 */
+		
+		System.out.println();
+		System.out.println(risto.stream().sorted(Comparator.comparing(Ristorante::getCoperti)).collect(Collectors.groupingBy(Ristorante::getTipo)));
+
+		 /* 5)Data la lista dei ristoranti, scrivere un metodo che stampi i nomi dei ristoranti in ordine 
+		 *   alfabetico separati da virgola.
+		 */   
+		risto.stream().sorted(Comparator.comparing(Ristorante::getNome))
+		.forEach(r -> System.out.println(r.getNome() + ","));
+		
+		 /* 6)Data la lista dei ristoranti,scrivere un metodo che restituisca la somma totale di tutti i coperti
+		 *   dei ristorsnti della lista.
+		 */   
+		
+		System.out.println();
+		System.out.println(risto.stream().map(Ristorante::getCoperti).reduce(0, (a,b) -> a+b));
+		
+		 /* 7)Data la lista dei ristoranti,scrivere un metodo che restituisca il massimo, il minimo e la media 
+		 *   dei coperti.
+		 */
+		System.out.println();
+		IntSummaryStatistics stats = risto.stream().mapToInt(x -> x.getCoperti()).summaryStatistics();
+		System.out.println("Min " + stats.getMin());
+		System.out.println("Max " +stats.getMax());
+		System.out.println("Media " + stats.getAverage());
+		 
+		/* 8)Data la lista dei ristoranti, stampare la lista dei ristoranti con nome che inizia con D e
+		 *   lunghi almeno 7 lettere.
+		 */   
+		
+		System.out.println();
+		System.out.println(risto.stream().filter(s-> s.getNome().startsWith("D") && s.getNome().length()>=7 ).collect(Collectors.toList()));
+		
+		 /* 9)Calcolare la somma del doppio dei valori pari dei coperti dei ristoranti della lista.
+		 */ 
+		 
+		System.out.println();
+		System.out.println(risto.stream().map(Ristorante::getCoperti).filter(e->e%2 == 0).map(e -> e*2).reduce(0,Integer::sum));
+		
 
 	}
 }
