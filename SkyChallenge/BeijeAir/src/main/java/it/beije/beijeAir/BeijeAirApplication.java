153420c4f114a3450a95925e9cf4667df150d922
MariannaNido <marianna.nido@live.it>
2023-05-12 11:40:25 +0200

    BeijeAir

---
+++ SkyChallenge/BeijeAir/src/main/java/it/beije/beijeAir/BeijeAirApplication.java
@@ -0,0 +1,13 @@
+package it.beije.beijeAir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeijeAirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeijeAirApplication.class, args);
+	}
+
+}
---
+++ SkyChallenge/BeijeAir/src/test/java/it/beije/beijeAir/BeijeAirApplicationTests.java
@@ -0,0 +1,13 @@
+package it.beije.beijeAir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeijeAirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
