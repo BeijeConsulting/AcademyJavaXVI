3eb7ab45aabf871448cc767e9b8a451877b4b83b
nverzaschi <nverzaschi@gmail.com>
2023-05-12 14:35:25 +0200

    controller and service added

---
+++ SkyChallenge/BeijeJet/src/main/java/it/beije/beijeJet/controller/SearchController.java
@@ -0,0 +1,8 @@
+package it.beije.beijeJet.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SearchController {
+
+}
---
+++ SkyChallenge/BeijeJet/src/main/java/it/beije/beijeJet/service/SearchService.java
@@ -0,0 +1,8 @@
+package it.beije.beijeJet.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SearchService {
+
+}
