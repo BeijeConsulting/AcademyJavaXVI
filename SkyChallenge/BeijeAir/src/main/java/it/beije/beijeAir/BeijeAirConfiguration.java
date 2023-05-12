0fd9bee26d3fad8a3c9dcddaab0801775fd55bc0
MariannaNido <marianna.nido@live.it>
2023-05-12 12:18:57 +0200

    init

---
+++ SkyChallenge/BeijeAir/src/main/java/it/beije/beijeAir/BeijeAirConfiguration.java
@@ -0,0 +1,29 @@
+package it.beije.beijeAir;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableAsync
+@EnableScheduling
+@EnableTransactionManagement
+public class BeijeAirConfiguration {
+	
+	@Primary
+	@Bean(name = "transactionManager")
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+		return transactionManager;
+	}
+
+}
