fa555bca4b1e1d60c6f5124edad45f3ae051a180
nverzaschi <nverzaschi@gmail.com>
2023-05-12 10:40:11 +0200

    BeijeJet added

---
+++ SkyChallenge/BeijeJet/beijeJet/src/main/java/it/beije/beijeJet/BeijeJetApplication.java
@@ -0,0 +1,13 @@
+package it.beije.beijeJet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeijeJetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeijeJetApplication.class, args);
+	}
+
+}
---
+++ SkyChallenge/BeijeJet/beijeJet/src/main/java/it/beije/beijeJet/ServletInitializer.java
@@ -0,0 +1,13 @@
+package it.beije.beijeJet;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BeijeJetApplication.class);
+	}
+
+}
