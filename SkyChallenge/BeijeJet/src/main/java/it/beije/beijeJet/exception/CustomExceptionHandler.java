f2b2bf92c0ab0038a8afbda8d242b9a7a32911b8
nverzaschi <nverzaschi@gmail.com>
2023-05-15 10:37:29 +0200

    fix

--- SkyChallenge/BeijeJet/src/main/java/it/beije/beijeJet/exception/CustomExceptionHandler.java
+++ SkyChallenge/BeijeJet/src/main/java/it/beije/beijeJet/exception/CustomExceptionHandler.java
@@ -9,8 +9,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import it.beije.shoes.exception.ErrorMessage;
+import it.beije.beijeJet.exception.ErrorMessage;
 
 /*	RIEPILOGO GESTIONE ECCEZIONI:
  * 
