35a8681d6f1083326e50ee98fe8fa176ec4a25e6
Levva01 <vellanifederico13@gmail.com>
2023-05-15 09:54:01 +0200

    + ricercaVoli

--- SkyChallenge/BeijeAir/src/main/java/it/beije/beijeAir/controller/VoliController.java
+++ SkyChallenge/BeijeAir/src/main/java/it/beije/beijeAir/controller/VoliController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import it.beije.beijeAir.model.Voli;
 import it.beije.beijeAir.service.VoliService;
@@ -27,8 +29,20 @@ public class VoliController {
 	}
 	
 	@GetMapping(value="/")
-	public String getIndex() {
+	public String getIndex(Model model) {
+		model.addAttribute("SearchDTO", new SearchDTO());
 	    return "index";
+	}
+	
+	@PostMapping("/ricercaVoli")
+	public String searchFlights(@ModelAttribute("SearchDTO") SearchDTO searchDTO, Model model) {
+
+		//TODO ricerca utilizzando i dati del DTO
+		
+		//aggiunge i risultati alla model per passarli alla pagina dei risultati
+		model.addAttribute("flightResults", flightResults);
+		
+	    return "ricercaVoli";
 	}
 
 }
