cdab14b1f98bb3867dde31a54cc8193602dc81dd
mongipi <pietromongiello@gmail.com>
2023-05-15 15:41:19 +0200

    add data ritorno

--- SkyChallenge/BeijeAir/src/main/java/it/beije/beijeAir/service/VoliService.java
+++ SkyChallenge/BeijeAir/src/main/java/it/beije/beijeAir/service/VoliService.java
@@ -83,7 +83,7 @@ public class VoliService {
 			searchDto.setCittaArrivo(searchDto.getCittaPartenza());
 			searchDto.setCittaPartenza(appo);
 			searchDto.setDataPartenza(searchDto.getDataRitorno());
-			// TODO: data di arrivo metterla in data di partenza
+			
 			rotte.addAll(find(searchDto));
 		}		
 				
