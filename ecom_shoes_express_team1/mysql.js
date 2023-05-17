4cb9ca8824f47b12db198def4a18366c2bf70aab
francescoandreotti <francescoandreotti00@gmail.com>
2023-05-17 14:14:42 +0200

    mysql

---
+++ ecom_shoes_express_team1/mysql.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'ecommerce_shoes_3'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('An error occurred while connecting to the DB')
+    throw err
+  }
+  console.log('Connected successfully to the DB');
+});
+
+connection.end()
