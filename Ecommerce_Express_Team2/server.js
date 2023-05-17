809f8a06b189d19dbf2acf4ffe8c2ffe55ff391f
MariannaNido <marianna.nido@live.it>
2023-05-17 18:32:39 +0200

    Login ok

--- Ecommerce_Express_Team2/server.js
+++ Ecommerce_Express_Team2/server.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const mysql = require('mysql')
-
 const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const cookieParser = require('cookie-parser');
 
-//
+// DATABASE
 const port = 3000
 const connection = mysql.createConnection({
   host: 'skychallenge.cvzu4xrxvkdz.eu-south-1.rds.amazonaws.com',
@@ -14,79 +12,76 @@ const connection = mysql.createConnection({
   database: 'ecommerce_shoes_2'
 })
 
+app.use(cookieParser());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.set('view engine', 'ejs')
+
+// SIGN IN
+app.get('/signin', (req, res) => {
+  res.render('db3/signin')
+})
+
+app.post('/signin', (req, res) => {
+  let password = req.body.password;
+  let email = req.body.email;
+  connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], (err, rows) => {
+    if (err) throw err
+    res.cookie('id', parseInt(rows[0].id), { maxAge: 900000, httpOnly: true });
+    res.redirect('/user')
+  }) //sistemare credenziali errate
+
+})
+
 
 app.get('/users', (req, res) => {
 
-    //connection.connect()
-    
-    connection.query('SELECT * FROM users', (err, rows) => {
-        if (err) throw err
-        console.log('rows: ', rows)
-        //res.render('contacts', { contacts: rows })
-    })
-
-    console.log('end connection...')
-    //connection.end()
-    
-  })
-  app.get('/user', (req, res) => {
-
-    //connection.connect()
-    let id = parseInt(req.query.id);
-    console.log('id: ', id)
-    connection.query('SELECT * FROM users WHERE id = ?', [id], (err, rows) => {
-        if (err) throw err
-        console.log('rows: ', rows)
-        //res.render('contacts', { contacts: rows })
-    })
-    console.log('end connection...')
-    //connection.end()
-    
+  connection.query('SELECT * FROM users', (err, rows) => {
+    if (err) throw err
+    console.log('rows: ', rows)
   })
 
-  app.post('/signup', (req, res) => {
-    let password = req.body.password;
-    let email = req.body.email;
-    let name = req.body.name;
-    let telephone = req.body.telephone;
-    let birth_date = req.body.birthdate;
-    let surname = req.body.surname;
-    connection.query('INSERT INTO users (name, surname, email, password, telephone, birth_date) VALUES (?, ?, ?, ?, ?, ?)', [name, surname, email, password, telephone, birth_date], (err, rows) => {
-        if (err) throw err
-        console.log('rows: ', rows)
-        //res.render('contacts', { contacts: rows })
-  })
-  
+})
+
+
+app.get('/user', (req, res) => {
+  let id = parseInt(req.cookies.id)
+
+  connection.query('SELECT * FROM users WHERE id = ?', [id], (err, rows) => {
+    if (err) throw err
+    res.render('db3/user/user_page', { user: rows[0] })
   })
 
-  app.post('/login', (req, res) => {
-    console.log(req.body)
-    let password = req.body.password;
-    let email = req.body.email;
-    connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], (err, rows) => {
-        if (err) throw err
-        console.log('rows: ', rows)
-        //res.render('contacts', { contacts: rows })
-    })
-   
+})
+
+app.post('/signup', (req, res) => {
+  let password = req.body.password;
+  let email = req.body.email;
+  let name = req.body.name;
+  let telephone = req.body.telephone;
+  let birth_date = req.body.birthdate;
+  let surname = req.body.surname;
+  connection.query('INSERT INTO users (name, surname, email, password, telephone, birth_date) VALUES (?, ?, ?, ?, ?, ?)', [name, surname, email, password, telephone, birth_date], (err, rows) => {
+    if (err) throw err
+    console.log('rows: ', rows)
+    //res.render('contacts', { contacts: rows })
   })
 
-  app.get('/products', (req, res) => {
-    connection.query('SELECT * FROM products', (err, rows) => {
-        if (err) throw err
-        console.log('rows: ', rows)
-        let products = rows;
-        res.render('db3/index', { products: products })
-        //res.render('contacts', { contacts: rows })
-    })
-   
-  })
-//app.get('/addresses', (req, res) => {
-//    console.log('end connection...')
-//    //connection.end()
-//}
+})
 
+app.get('/products', (req, res) => {
+  connection.query('SELECT * FROM products', (err, rows) => {
+    if (err) throw err
+    console.log('rows: ', rows)
+    let products = rows;
+    res.render('db3/index', { products: products })
+    //res.render('contacts', { contacts: rows })
+  })
+
+})
+
+// LISTEN
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
