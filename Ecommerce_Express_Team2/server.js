33ec13c917719b9219764f7d146acec56a0596c1
youn <mm@00>
2023-05-17 12:50:46 +0200

    Init

---
+++ Ecommerce_Express_Team2/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mysql = require('mysql')
+
+const app = express()
+const port = 3000
+const connection = mysql.createConnection({
+  host: 'skychallenge.cvzu4xrxvkdz.eu-south-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'FJf7MWgOsW1M5AbKql9g',
+  database: 'ecommerce_shoes_2'
+})
+
+app.set('view engine', 'ejs')
+
+app.get('/contacts', (req, res) => {
+
+    //connection.connect()
+    
+    connection.query('SELECT * FROM contatti', (err, rows) => {
+        if (err) throw err
+        console.log('rows: ', rows)
+        res.render('contacts', { contacts: rows })
+    })
+
+    console.log('end connection...')
+    //connection.end()
+    
+  })
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
