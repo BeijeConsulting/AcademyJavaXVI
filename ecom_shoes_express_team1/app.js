748f6d298286e9156651c61ce3c2247b7358c18e
francescoandreotti <francescoandreotti00@gmail.com>
2023-05-17 14:39:13 +0200

    users.ejs

--- ecom_shoes_express_team1/app.js
+++ ecom_shoes_express_team1/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 3000
 const db = require('./db');
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -12,15 +15,10 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-app.get('/test', (req, res) => {
-
-  
+app.get('/users', (req, res) => {
   db.query('SELECT * FROM users', (err, rows) => {
       if (err) throw err
       console.log('rows: ', rows)
       res.render('users', { users: rows })
   })
-
-  console.log('end connection...')
-  
 })
