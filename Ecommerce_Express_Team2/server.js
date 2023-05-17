d6fbe0bc9189549d38e22f26f01830497d97f61c
youn <mm@00>
2023-05-17 15:15:56 +0200

    lasignupfunziona

--- Ecommerce_Express_Team2/server.js
+++ Ecommerce_Express_Team2/server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const mysql = require('mysql')
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//
 const port = 3000
 const connection = mysql.createConnection({
   host: 'skychallenge.cvzu4xrxvkdz.eu-south-1.rds.amazonaws.com',
@@ -25,6 +29,47 @@ app.get('/users', (req, res) => {
     console.log('end connection...')
     //connection.end()
     
+  })
+  app.get('/user', (req, res) => {
+
+    //connection.connect()
+    let id = parseInt(req.query.id);
+    console.log('id: ', id)
+    connection.query('SELECT * FROM users WHERE id = ?', [id], (err, rows) => {
+        if (err) throw err
+        console.log('rows: ', rows)
+        //res.render('contacts', { contacts: rows })
+    })
+    console.log('end connection...')
+    //connection.end()
+    
+  })
+
+  app.post('/signup', (req, res) => {
+    let password = req.body.password;
+    let email = req.body.email;
+    let name = req.body.name;
+    let telephone = req.body.telephone;
+    let birth_date = req.body.birthdate;
+    let surname = req.body.surname;
+    connection.query('INSERT INTO users (name, surname, email, password, telephone, birth_date) VALUES (?, ?, ?, ?, ?, ?)', [name, surname, email, password, telephone, birth_date], (err, rows) => {
+        if (err) throw err
+        console.log('rows: ', rows)
+        //res.render('contacts', { contacts: rows })
+  })
+  
+  })
+
+  app.post('/login', (req, res) => {
+    console.log(req.body)
+    let password = req.body.password;
+    let email = req.body.email;
+    connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], (err, rows) => {
+        if (err) throw err
+        console.log('rows: ', rows)
+        //res.render('contacts', { contacts: rows })
+    })
+   
   })
 //app.get('/addresses', (req, res) => {
 //    console.log('end connection...')
