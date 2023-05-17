f50ac3d140dfbc112ecd9e5c08109d2eb07dcd5b
francescoandreotti <francescoandreotti00@gmail.com>
2023-05-17 14:31:16 +0200

    db connection

--- ecom_shoes_express_team1/app.js
+++ ecom_shoes_express_team1/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const db = require('./db');
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -8,4 +10,17 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/test', (req, res) => {
+
+  
+  db.query('SELECT * FROM users', (err, rows) => {
+      if (err) throw err
+      console.log('rows: ', rows)
+      res.render('users', { users: rows })
+  })
+
+  console.log('end connection...')
+  
 })
