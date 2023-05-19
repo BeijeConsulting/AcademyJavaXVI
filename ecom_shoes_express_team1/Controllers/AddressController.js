42ccccece10670422d0468319c7e393aaef43d20
Levva01 <vellanifederico13@gmail.com>
2023-05-19 10:14:07 +0200

    edit addresses added

--- ecom_shoes_express_team1/Controllers/AddressController.js
+++ ecom_shoes_express_team1/Controllers/AddressController.js
@@ -14,14 +14,52 @@ exports.address = (req, res) => {
 };
 
 exports.deleteAddress = (req, res) => {
-    const addressId = req.params.id;
-  
-    db.query('DELETE FROM addresses WHERE id = ?', [addressId], (err, result) => {
+  const addressId = req.params.id;
+
+  db.query('DELETE FROM addresses WHERE id = ?', [addressId], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Errore durante l\'eliminazione dell\'indirizzo.');
+    } else {
+      res.redirect('/address');
+    }
+  });
+};
+
+exports.showUpdateForm = (req, res) => {
+  const addressId = req.params.id;
+
+  db.query('SELECT * FROM addresses WHERE id = ?', [addressId], (err, result) => {
       if (err) {
-        console.error(err);
-        res.status(500).send('Errore durante l\'eliminazione dell\'indirizzo.');
+          console.error(err);
+          res.status(500).send('Errore nel recupero dei dati dal database');
       } else {
-        res.redirect('/address');
+          const address = result[0];
+          res.render('editAddress', { address });
       }
-    });
+  });
+};
+
+exports.updateAddress = (req, res) => {
+  const addressId = req.params.id;
+  const { label, name_surname, country, street_address, telephone, zipcode, instructions } = req.body;
+
+  const updateData = {
+      label,
+      name_surname,
+      country,
+      street_address,
+      telephone,
+      zipcode,
+      instructions
+  };
+
+  db.query('UPDATE addresses SET ? WHERE id = ?', [updateData, addressId], (err, result) => {
+      if (err) {
+          console.error(err);
+          res.status(500).send('Errore durante l\'aggiornamento dell\'indirizzo.');
+      } else {
+          res.redirect('/address');
+      }
+  });
 };
--- ecom_shoes_express_team1/routes/addressRoutes.js
+++ ecom_shoes_express_team1/routes/addressRoutes.js
@@ -4,5 +4,8 @@ const addressController = require('../Controllers/AddressController');
 
 router.get('/', addressController.address);
 router.post('/delete/:id', addressController.deleteAddress);
+router.get('/edit/:id', addressController.showUpdateForm);
+router.post('/update/:id', addressController.updateAddress);
+
 
 module.exports = router;
