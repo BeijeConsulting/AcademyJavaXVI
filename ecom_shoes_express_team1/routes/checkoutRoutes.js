5696f5dee89b53a5b7f55197f1ad86238531064e
francescoandreotti <francescoandreotti00@gmail.com>
2023-05-19 15:33:09 +0200

    fix

--- ecom_shoes_express_team1/routes/checkoutRoutes.js
+++ ecom_shoes_express_team1/routes/checkoutRoutes.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-const checkoutController = require('../controllers/checkoutController');
+const checkoutController = require('../Controllers/checkoutController');
 
 
 router.get('/', checkoutController.getCheckout);
