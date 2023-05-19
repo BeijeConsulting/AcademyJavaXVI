29ffc3be1fa5bb68c2802e550a52807e5bcace68
gianfra097 <giandesign1997@gmail.com>
2023-05-19 13:02:28 +0200

    Shopping cart funzionante

--- ecommerce_shoes_express_team3/routes/cart.js
+++ ecommerce_shoes_express_team3/routes/cart.js
@@ -1,6 +1,10 @@
 const express=require('express')
 const router=express.Router()
 const mysql = require('mysql')
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const connection = mysql.createConnection({
   host: 'skychallenge.cvzu4xrxvkdz.eu-south-1.rds.amazonaws.com',
@@ -11,8 +15,17 @@ const connection = mysql.createConnection({
 
 router.get('/', (req, res) => {
   //connection.connect()
+  let token = req.cookies.session
+  let model = {}
+  if (token !== undefined){
+    jwt.verify(token, process.env.KEY, function(err, decoded) {
+      if (!err) {
+        model.user = decoded.user_id
+      }
+    })
+  }
 
-  connection.query('SELECT DISTINCT * FROM shopping_cart_item,product_details,products WHERE shopping_cart_item.product_details_id = product_details.id AND product_details.product_id = products.id', (err, rows) => {
+  connection.query('SELECT * FROM shopping_cart_item,product_details,products WHERE shopping_cart_item.user_id = ? AND product_details_id = product_details.id AND product_details.product_id = products.id', [model.user], (err, rows) => {
       if (err) throw err
       console.log('rows: ', rows)
       res.render('cart', { carts: rows })
